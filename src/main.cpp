09f37f49bb92bec3dfbbd17ace949a3c0ecf415d
Ricky Lu <ricky_lu0@outlook.com>
2024-05-07 17:17:53 -0400

    All 4 screens implemented

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@ Adafruit_SSD1306 * display_ptr = &display;
 
 int buttonState = 1;  // variable for reading the pushbutton status
 int buttonPrevState = 1;
+bool screenLock = false;  // Lock Screen display data
 
 // For time tracking
 unsigned long lastScreenChanged = millis();
@@ -36,6 +37,7 @@ DHT dht(DHTPIN, DHTTYPE);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  pmsSerial.begin(9600);
 
   pinMode(BUTTON_PIN, INPUT_PULLUP);  // Sets Button as input
 
@@ -68,6 +70,8 @@ void loop() {
     } else {
       // if the current state is LOW then the button
       // wend from on to off:
+      screenLock = !screenLock; // Toggle screen lock
+      lastScreenChanged = millis();
       tone(BUZZER_PIN, 1567, 50); // Plays sound when pressed
 
   delay(100);
@@ -80,14 +84,42 @@ void loop() {
   buttonPrevState = buttonState;
 // =======================================================
 
+//================ Handles AQI measurements ==============================
+if (readPMSdata(&pmsSerial)) {
+    // reading data was successful!
+    Serial.println();
+    Serial.println("---------------------------------------");
+    Serial.println("Concentration Units (standard)");
+    Serial.print("PM 1.0: "); Serial.print(data.pm10_standard);
+    Serial.print("\t\tPM 2.5: "); Serial.print(data.pm25_standard);
+    Serial.print("\t\tPM 10: "); Serial.println(data.pm100_standard);
+    Serial.println("---------------------------------------");
+    Serial.println("Concentration Units (environmental)");
+    Serial.print("PM 1.0: "); Serial.print(data.pm10_env);
+    Serial.print("\t\tPM 2.5: "); Serial.print(data.pm25_env);
+    Serial.print("\t\tPM 10: "); Serial.println(data.pm100_env);
+    Serial.println("---------------------------------------");
+    Serial.print("Particles > 0.3um / 0.1L air:"); Serial.println(data.particles_03um);
+    Serial.print("Particles > 0.5um / 0.1L air:"); Serial.println(data.particles_05um);
+    Serial.print("Particles > 1.0um / 0.1L air:"); Serial.println(data.particles_10um);
+    Serial.print("Particles > 2.5um / 0.1L air:"); Serial.println(data.particles_25um);
+    Serial.print("Particles > 5.0um / 0.1L air:"); Serial.println(data.particles_50um);
+    Serial.print("Particles > 10.0 um / 0.1L air:"); Serial.println(data.particles_100um);
+    Serial.println("---------------------------------------");
+  }
+
+
 //================ Handles Screen Content ================
   curTime = millis();
   if(curTime - lastScreenChanged > delayTime)
   {
     char(content)[8];
-    curScreen += 1;
-    if(curScreen >= 5) { curScreen = 0; }
-    lastScreenChanged = millis();
+    if(!screenLock) {
+      curScreen += 1;
+      if(curScreen >= 5) { curScreen = 0; }
+      lastScreenChanged = millis();
+    }
+
     switch(curScreen)
     {
       case 0:
@@ -111,8 +143,8 @@ void loop() {
 
       case 2:
       {
-        float pm2_5 = 50;
-        char(title)[12] = "PM2.5 ug/L";
+        float pm2_5 = data.pm25_standard;
+        char(title)[12] = "PM2.5ug/m3";
         sprintf(content, "%.1f", pm2_5);
         TwolineDisplay(display_ptr, &title, &content);
         break;
@@ -120,18 +152,25 @@ void loop() {
 
       case 3:
       {
-        float pm10 = 50;
-        char(title)[12] = "PM10";
-        sprintf(content, "%.0f", pm10);
+        float pm10 = data.pm10_env;
+        char(title)[12] = "PM10 ug/m3";
+        sprintf(content, "%.1f", pm10);
         TwolineDisplay(display_ptr, &title, &content);
+
+
         break;
       }
 
       case 4:
       {
-        float aqi = 30;
-        char(title)[12] = "AQI";
-        sprintf(content, "%.0f", aqi);
+        char(title)[12] = "Air Qlty.";
+        float pm2_5 = data.pm25_standard;
+        if(pm2_5 <= 12){sprintf(content, "%s", "Good");}
+        else if(pm2_5 <= 35.4){sprintf(content, "%s", "Mod.");}
+        else if(pm2_5 <= 55.4){sprintf(content, "%s", "Unhty 1");}
+        else if(pm2_5 <= 150.4){sprintf(content, "%s", "Unhty 2");}
+        else if(pm2_5 <= 250.4){sprintf(content, "%s", "Unhty 3");}
+        else if(pm2_5 > 250.4){sprintf(content, "%s", "Hzrd");}
         TwolineDisplay(display_ptr, &title, &content);
         break;
       }
@@ -178,4 +217,54 @@ void TwolineDisplay(Adafruit_SSD1306 * d, char(*title)[12], char(*content)[8])
   // display.print(content);
   // display.display();
 
+}
+
+boolean readPMSdata(Stream *s) {
+  if (! s->available()) {
+    return false;
+  }
+  
+  // Read a byte at a time until we get to the special '0x42' start-byte
+  if (s->peek() != 0x42) {
+    s->read();
+    return false;
+  }
+ 
+  // Now read all 32 bytes
+  if (s->available() < 32) {
+    return false;
+  }
+    
+  uint8_t buffer[32];    
+  uint16_t sum = 0;
+  s->readBytes(buffer, 32);
+ 
+  // get checksum ready
+  for (uint8_t i=0; i<30; i++) {
+    sum += buffer[i];
+  }
+ 
+  /* debugging
+  for (uint8_t i=2; i<32; i++) {
+    Serial.print("0x"); Serial.print(buffer[i], HEX); Serial.print(", ");
+  }
+  Serial.println();
+  */
+  
+  // The data comes in endian'd, this solves it so it works on all platforms
+  uint16_t buffer_u16[15];
+  for (uint8_t i=0; i<15; i++) {
+    buffer_u16[i] = buffer[2 + i*2 + 1];
+    buffer_u16[i] += (buffer[2 + i*2] << 8);
+  }
+ 
+  // put it into a nice struct :)
+  memcpy((void *)&data, (void *)buffer_u16, 30);
+ 
+  if (sum != data.checksum) {
+    Serial.println("Checksum failure");
+    return false;
+  }
+  // success!
+  return true;
 }
--- src/main.h
+++ src/main.h
@@ -8,7 +8,19 @@
 // - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
 // - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
 #include <DHT.h>
+#include <SoftwareSerial.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// PMS5003 adopted from: https://how2electronics.com/interfacing-pms5003-air-quality-sensor-arduino/
+SoftwareSerial pmsSerial(16, 17);
+struct pms5003data {
+  uint16_t framelen;
+  uint16_t pm10_standard, pm25_standard, pm100_standard;
+  uint16_t pm10_env, pm25_env, pm100_env;
+  uint16_t particles_03um, particles_05um, particles_10um, particles_25um, particles_50um, particles_100um;
+  uint16_t unused;
+  uint16_t checksum;
+};
+struct pms5003data data;
+
 void TwolineDisplay(Adafruit_SSD1306*, char(*)[12], char(*)[8]);
+boolean readPMSdata(Stream *);
