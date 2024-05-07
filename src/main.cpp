36d4b0bed5ee822e238de759d6488e462921eb0e
Ricky Lu <ricky_lu0@outlook.com>
2024-05-07 13:08:31 -0400

    screen rotates, filled with dummy var

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,5 @@
 #include "main.h"
-#include <Arduino.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 
-// DHT referenced: ladyada
-// REQUIRES the following Arduino libraries:
-// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
-// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
-#include <DHT.h>
 
 #define DHTPIN 4     // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
@@ -21,7 +12,26 @@
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+Adafruit_SSD1306 * display_ptr = &display;
 
+int buttonState = 1;  // variable for reading the pushbutton status
+int buttonPrevState = 1;
+
+// For time tracking
+unsigned long lastScreenChanged = millis();
+unsigned long delayTime = 3000;
+unsigned long curTime = 0;
+
+
+// For tracking screen switching
+int curScreen = 0; // 0 = Temperature, 1 = Humidity, 2 = PM2.5, 3 = PM 10, 4 = AQI
+
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   // put your setup code here, to run once:
@@ -34,24 +44,139 @@ void setup() {
     for(;;);
   }
 
+  delay(2000);
+  
+  dht.begin();
+
+
   Serial.println("Setup Complete!");
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //================ Handles button events ================
+  // read the pushbutton input pin:
+  buttonState = digitalRead(BUTTON_PIN);
+  // compare the buttonState to its previous state
+  if (buttonState != buttonPrevState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH then the button
+      // wend from off to on:
 
-  display.clearDisplay();
-  display.setCursor(0, 0); 
-  display.setTextColor(WHITE);
-  display.setTextSize(2);
-  display.println("dfsdsf");
-  display.println(myFunction(1,2));
-  display.display();
+  delay(100);
+    } else {
+      // if the current state is LOW then the button
+      // wend from on to off:
+      curScreen += 1;
+      tone(BUZZER_PIN, 1567, 50); // Plays sound when pressed
+
+  delay(100);
+    }
+    // Delay a little bit to avoid bouncing
+    //delay(50);
+  }
+  // save the current state as the last state,
+  //for next time through the loop
+  buttonPrevState = buttonState;
+// =======================================================
+
+//================ Handles Screen Content ================
+  curTime = millis();
+  if(curTime - lastScreenChanged > delayTime)
+  {
+    Serial.println(lastScreenChanged);
+    char(content)[8];
+    curScreen += 1;
+    if(curScreen >= 5) { curScreen = 0; }
+    lastScreenChanged = millis();
+    switch(curScreen)
+    {
+      case 0:
+      {
+        // Read temperature as Fahrenheit
+        float t = dht.readTemperature(true);
+        char(title)[12] = "Temp";
+        sprintf(content, "%.0f", t);
+        TwolineDisplay(display_ptr, &title, &content);
+        break;
+      }
+
+      case 1:
+      {
+        float h = dht.readHumidity();
+        char(title)[12] = "Hum:";
+        sprintf(content, "%.0f", h);
+        TwolineDisplay(display_ptr, &title, &content);
+        break;
+      }
+
+      case 2:
+      {
+        float pm2_5 = 50;
+        char(title)[12] = "PM2.5:";
+        sprintf(content, "%.0f", pm2_5);
+        TwolineDisplay(display_ptr, &title, &content);
+        break;
+      }
+
+      case 3:
+      {
+        float pm10 = 50;
+        char(title)[12] = "PM10:";
+        sprintf(content, "%.0f", pm10);
+        TwolineDisplay(display_ptr, &title, &content);
+        break;
+      }
+
+      case 4:
+      {
+        float aqi = 30;
+        char(title)[12] = "AQI:";
+        sprintf(content, "%.0f", aqi);
+        TwolineDisplay(display_ptr, &title, &content);
+        break;
+      }
+    }
+  }
 }
 
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
+}
+
+/*
+Displays 2 lines of content on SSD 1306 128x64 OLED Display
+display: an instance of Adafruit_SSD1306 to be used
+title: the first line on the display, yellow
+content: the second line on the display, blue
+*/
+void TwolineDisplay(Adafruit_SSD1306 * d, char(*title)[12], char(*content)[8])
+{
+  
+  d->clearDisplay();
+  d->setCursor(0, 0); 
+  d->setTextColor(WHITE);
+  d->setTextSize(2);
+  d->println(*title);
+
+  d->setTextSize(1);
+  d->println();
+
+  d->setTextSize(3);
+  d->println(*content);
+  d->display();
+
+
+  // display.setTextSize(1);
+  // display.println();
+  // display.display();
+
+
+  // display.setTextSize(3);
+  // display.print(content);
+  // display.display();
+
 }
--- src/main.h
+++ src/main.h
@@ -1,4 +1,14 @@
 #pragma once
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+// DHT referenced: ladyada
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+#include <DHT.h>
 
 // put function declarations here:
 int myFunction(int, int);
+void TwolineDisplay(Adafruit_SSD1306*, char(*)[12], char(*)[8]);
